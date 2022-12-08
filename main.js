8b74cc28b2969a82baa920f1fd80d0baf17fc41e
dragvn <liminalinquiries@gmail.com>
2022-12-08 10:13:23 -0500

    -jsway arrays problems

--- main.js
+++ main.js
@@ -2,8 +2,59 @@
 SORT ARRAY IN ASCENDING ORDER NUMERICALLY 
 **********************/
 
-    function sortArrayAscending(array){
-        let sortedArray = array.sort((a,b)=> a-b)
-        console.log(sortedArray)
-    }
-    sortArrayAscending([34,12,90])
+    // function sortArrayAscending(array){
+    //     let sortedArray = array.sort((a,b)=> a-b)
+    //     console.log(sortedArray)
+    // }
+    // sortArrayAscending([34,12,90])
+
+    // function sortInAcscending(array){
+    //     return array.sort((a,b)=> a-b)
+    // }
+
+/***************************************************** */
+// Musketeers
+// Write a program that:
+    
+// Creates an array named musketeers containing values "Athos", "Porthos" and "Aramis".
+// Shows each array element using a for loop.
+// Adds the "D'Artagnan" value to the array.
+// Shows each array element using the forEach() method.
+// Remove poor Aramis.
+// Shows each array element using a for-of loop.
+    // let musketeers = ['Athos', 'Porthos', 'Aramis']
+    // for(i=0;i<=musketeers.length-1;i++){
+    //     console.log(musketeers[i])
+    // }
+    // musketeers.push("D'Artagnan")
+    // musketeers.forEach(musketeer => console.log(musketeer))
+    // musketeers.splice(2,1)
+    // for(const musketeer of musketeers){
+    //     console.log(musketeer)
+    // }
+
+/********************************************************* */
+// Sum of values
+// Write a program that creates the following array, then calculates and shows the sum of its values (42 in that case).
+
+// const values = [3, 11, 7, 2, 9, 10];
+
+    // let values = [3, 11, 7, 2, 9, 10]
+    // let sum = 0
+    // values.forEach(value => sum += value)
+    // console.log(sum)
+
+/********************************************************* */
+// Array maximum
+// Write a program that creates the following array, then calculates and shows the array's maximum value.
+
+// const values = [3, 11, 7, 2, 9, 10];
+
+    // let values = [3, 11, 7, 2, 9, 10]
+    // let sortedValues = values.sort((a,b)=> a-b)
+    // console.log('The arrays maximum values is ' + sortedValues[sortedValues.length-1])
+
+/********************************************************* */
+// List of words
+// Write a program that asks the user for a word until the user types "stop". The program then shows each of these words, except "stop".
+
