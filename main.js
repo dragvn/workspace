1aec90445504263f9c9316b018391e19678b5900
dragvn <liminalinquiries@gmail.com>
2022-12-10 11:30:43 -0500

    -camelize function

--- main.js
+++ main.js
@@ -159,4 +159,31 @@ SORT ARRAY IN ASCENDING ORDER NUMERICALLY
     // account.credit(-80)
     // console.log(account.describe())
 
-/************************************************************** */
+/************************************************************** *
+ARRAY METHODS
+*****************************************************************/
+//CAMEL CASE / CAMELIZE
+// Translate border-left-width to borderLeftWidth
+// importance: 5
+// Write the function camelize(str) that changes dash-separated words like “my-short-string” into camel-cased “myShortString”.
+
+// That is: removes all dashes, each word after dash becomes uppercased.
+
+// Examples:
+
+// camelize("background-color") == 'backgroundColor';
+// camelize("list-style-image") == 'listStyleImage';
+// camelize("-webkit-transition") == 'WebkitTransition';
+// P.S. Hint: use split to split the string into an array, transform it and join back.
+
+    // function camelize(str){
+    //     return str
+    //         .split('-')
+    //         .map((word,index) => index == 0 ? word: word[0].toUpperCase() + word.slice(1))
+    //         .join('')
+    // }
+
+    // console.log(camelize('background-color'))
+    // console.log(camelize('list-style-image'))
+
+/********************************************************** */
