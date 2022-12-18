491385e82002d6bb8d17dd8bd13f3c2aca829215
dragvn <liminalinquiries@gmail.com>
2022-12-17 19:56:08 -0500

    -array join

--- main.js
+++ main.js
@@ -411,3 +411,14 @@ ARRAY METHODS
     // let sw = new Stopwatch
 
 /*********************************************************** */
+//Write a function that takes an array of words and smashes them together into a sentence 
+//and returns the sentence. You can ignore any need to sanitize words or add punctuation, 
+//but you should add spaces between each word. 
+//Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+    // let words = ['hello', 'world', 'this', 'is', 'great']
+    // const smash = arr => arr.join(' ')
+
+    // console.log(smash(words))
+
+/************************************************************** */
