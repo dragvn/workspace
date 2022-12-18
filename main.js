771798ea01b078ad288729bb72391de2e1e29261
dragvn <liminalinquiries@gmail.com>
2022-12-17 20:36:10 -0500

    -multiply numeric property values only

--- main.js
+++ main.js
@@ -463,6 +463,21 @@ ARRAY METHODS
 // Should be 390 in the example above.
 // If salaries is empty, then the result must be 0.
 /*************************************************************** */
+// Multiply numeric property values by 2
+// Create a function multiplyNumeric(obj) that multiplies 
+//all numeric property values of obj by 2.
+    // let menu = {
+    //     width: 200,
+    //     height: 300,
+    //     title: 'My menu'
+    // }
+    // function multiplyNumeric(obj){
+    //     for(let key in obj){
+    //         if(typeof obj[key] === 'number'){
+    //             obj[key] *= 2
+    //         }
+    //     }
+    // }
 /*************************************************************** */
 /*************************************************************** */
 /*************************************************************** */
