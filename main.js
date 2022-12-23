3a8ae77660adfcc26a3b7436bb5d9fb6befcdbce
dragvn <liminalinquiries@gmail.com>
2022-12-23 14:38:52 -0500

    -string of numbers (return highest and lowest)

--- main.js
+++ main.js
@@ -528,6 +528,15 @@ ARRAY METHODS
     // }
     // console.log(nMulitplesOfX(3, 3))
 /*************************************************************** */
+// In this little assignment you are given a string of space separated numbers, 
+// and have to return the highest and lowest number
+let str = '1 2 3 4 5 6'
+function highAndLow(numbers){
+    let arr = numbers.split(' ').sort((a,b) => a - b)
+    return `${arr[arr.length-1]} ${arr[0]}`
+  }
+ console.log(highAndLow(str))
+  
 /*************************************************************** */
 /*************************************************************** */
 /*************************************************************** */
