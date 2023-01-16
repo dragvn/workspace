0c056e9d945b35777713bb67fd240c45003ed433
dragvn <liminalinquiries@gmail.com>
2023-01-16 10:01:18 -0500

    -jsOOP dog constructor

--- main.js
+++ main.js
@@ -667,7 +667,29 @@ ARRAY METHODS
 //filter out everything that isnt a vowel into a new arr
 //convert arr to string
 //return new string
-// /*************************************************************** */
+/*************************************************************** */
+    // class Dog {
+    //     constructor(name, species, size){
+    //         this.name = name
+    //         this.species = species
+    //         this.size = size
+    //     }
+    //     bark(){
+    //         if(this.height > 60){
+    //             return 'Grrr! Grrr!'
+    //         }else{
+    //             return 'Woof! Woof!'
+    //         }
+    //     }
+    // }
+
+    // const fang = new Dog("Fang", "boarhound", 75);
+    // console.log(`${fang.name} is a ${fang.species} dog measuring ${fang.size}`);
+    // console.log(`Look, a cat! ${fang.name} barks: ${fang.bark()}`);
+
+    // const snowy = new Dog("Snowy", "terrier", 22);
+    // console.log(`${snowy.name} is a ${snowy.species} dog measuring ${snowy.size}`);
+    // console.log(`Look, a cat! ${snowy.name} barks: ${snowy.bark()}`);
 /*************************************************************** */
 /*************************************************************** */
 /*************************************************************** */
