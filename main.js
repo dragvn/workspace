f428b80d7a90cece8caef0c83b90a559c41a972f
dragvn <liminalinquiries@gmail.com>
2023-01-16 10:18:07 -0500

    -comments

--- main.js
+++ main.js
@@ -702,63 +702,63 @@ ARRAY METHODS
 
 // When a character slays another character, the victim's inventory goes to its vanquisher.
 /*************************************************************** */
-class Character {
-    constructor(name, health, strength) {
-      this.name = name;
-      this.health = health;
-      this.strength = strength;
-      this.gold = 10; //start with 10
-      this.keys = 1; //start with 1
-      this.xp = 0; // XP is always zero for new characters
-    }
-    // Attack a target
-    attack(target) {
-      if (this.health > 0) {
-        const damage = this.strength;
-        console.log(
-          `${this.name} attacks ${target.name} and causes ${damage} damage points`
-        );
-        target.health -= damage;
-        if (target.health > 0) {
-          console.log(`${target.name} has ${target.health} health points left`);
-        } else {
-          target.health = 0;
-          const bonusXP = 10;
-          console.log(
-            `${this.name} eliminated ${target.name} and wins ${bonusXP} experience points, ${this.gold} gold and ${this.keys} key(s)`
-          );
-          this.xp += bonusXP;
-          this.gold += target.gold;
-          this.keys += target.keys;
-        }
-      } else {
-        console.log(`${this.name} can’t attack (they've been eliminated)`);
-      }
-    }
-    // Return the character description
-    describe() {
-      return `${this.name} has ${this.health} health points, ${this
-        .strength} as strength, ${this.xp} XP points, ${this.gold} gold and ${this.keys} key(s)`;
-    }
-  }
+    // class Character {
+    //     constructor(name, health, strength) {
+    //       this.name = name;
+    //       this.health = health;
+    //       this.strength = strength;
+    //       this.gold = 10; //start with 10
+    //       this.keys = 1; //start with 1
+    //       this.xp = 0; // XP is always zero for new characters
+    //     }
+    //     // Attack a target
+    //     attack(target) {
+    //       if (this.health > 0) {
+    //         const damage = this.strength;
+    //         console.log(
+    //           `${this.name} attacks ${target.name} and causes ${damage} damage points`
+    //         );
+    //         target.health -= damage;
+    //         if (target.health > 0) {
+    //           console.log(`${target.name} has ${target.health} health points left`);
+    //         } else {
+    //           target.health = 0;
+    //           const bonusXP = 10;
+    //           console.log(
+    //             `${this.name} eliminated ${target.name} and wins ${bonusXP} experience points, ${this.gold} gold and ${this.keys} key(s)`
+    //           );
+    //           this.xp += bonusXP;
+    //           this.gold += target.gold;
+    //           this.keys += target.keys;
+    //         }
+    //       } else {
+    //         console.log(`${this.name} can’t attack (they've been eliminated)`);
+    //       }
+    //     }
+    //     // Return the character description
+    //     describe() {
+    //       return `${this.name} has ${this.health} health points, ${this
+    //         .strength} as strength, ${this.xp} XP points, ${this.gold} gold and ${this.keys} key(s)`;
+    //     }
+    //   }
 
-const aurora = new Character("Aurora", 150, 25);
-const glacius = new Character("Glacius", 130, 30);
+    // const aurora = new Character("Aurora", 150, 25);
+    // const glacius = new Character("Glacius", 130, 30);
 
-console.log("Welcome to the adventure! Here are our heroes:");
-console.log(aurora.describe());
-console.log(glacius.describe());
+    // console.log("Welcome to the adventure! Here are our heroes:");
+    // console.log(aurora.describe());
+    // console.log(glacius.describe());
 
-const monster = new Character("Spike", 40, 20);
-console.log("A wild monster has appeared: it's named " + monster.name);
+    // const monster = new Character("Spike", 40, 20);
+    // console.log("A wild monster has appeared: it's named " + monster.name);
 
-monster.attack(aurora);
-monster.attack(glacius);
-aurora.attack(monster);
-glacius.attack(monster);
+    // monster.attack(aurora);
+    // monster.attack(glacius);
+    // aurora.attack(monster);
+    // glacius.attack(monster);
 
-console.log(aurora.describe());
-console.log(glacius.describe());
+    // console.log(aurora.describe());
+    // console.log(glacius.describe());
 /*************************************************************** */
 /*************************************************************** */
 /*************************************************************** */
