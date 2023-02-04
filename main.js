cfc3b43793ccbb090acc3bdb7b650d37ded97488
dragvn <liminalinquiries@gmail.com>
2023-02-04 07:30:34 -0500

    -cw pair zeros

--- main.js
+++ main.js
@@ -933,6 +933,10 @@ ARRAY METHODS
     // let arr = [n,m,o,p]
     // console.log(arr)
 /*************************************************************** */
+// For a given list of digits 0 to 9, return a list with the same digits in the same order, but with all 0s paired. 
+// Pairing two 0s generates one 0 at the location of the first one.
+// let arr = [0,1,0,2]
+// const pairZeros = (a,f) => a.filter(n => n || (f = !f))
 /*************************************************************** */
 /*************************************************************** */
 /*************************************************************** */
