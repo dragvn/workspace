21cb0a4ff959fb64fb26ef4008f39e2706d737e0
dragvn <liminalinquiries@gmail.com>
2023-03-09 12:40:00 -0500

    anki

--- main.js
+++ main.js
@@ -1368,6 +1368,13 @@ ARRAY METHODS
     // console.log(isCube(27, 3))
     // console.log(isCube(37, 3))
 /********************************************************************* */
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter.
+//Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+// If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
+
+    // const findNextSquare = n => Number.isInteger(Math.sqrt(n)) ? Math.pow(Math.sqrt(n)+1, 2) : -1
+    // console.log(findNextSquare(4))
+    // console.log(findNextSquare(5))
 /********************************************************************* */
 /********************************************************************* */
 /********************************************************************* */
