a77c61169d0ebf0145ae36e8a8218f0cb1326f1c
dragvn <liminalinquiries@gmail.com>
2023-03-10 07:33:07 -0500

    anki

--- main.js
+++ main.js
@@ -1376,8 +1376,22 @@ ARRAY METHODS
     // console.log(findNextSquare(4))
     // console.log(findNextSquare(5))
 /********************************************************************* */
+//RETURN THE CENTURY FOR A GIVEN YEAR
+    // const century = year => (Math.ceil(year/100))
+
+    // console.log(century(1200))
+    // console.log(century(1201))
 /********************************************************************* */
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter.
+//Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+// If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
+
+    // const findNextSquare = num => Number.isInteger(Math.sqrt(num)) ? Math.pow(Math.sqrt(num)+1, 2) : -1
+    // console.log(findNextSquare(4))
+    // console.log(findNextSquare(5))
 /********************************************************************* */
+//find nearest sqaure number to provided number
+    //const nearestSq = n => Math.power(Math.round(Math.sqrt(n)), 2)
 /********************************************************************* */
 /********************************************************************* */
 /********************************************************************* */
