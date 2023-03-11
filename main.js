bfba0b3aac31491016a74cf8370747a3eb9ab4ba
dragvn <liminalinquiries@gmail.com>
2023-03-11 07:25:58 -0500

    anki

--- main.js
+++ main.js
@@ -1393,7 +1393,29 @@ ARRAY METHODS
 //find nearest sqaure number to provided number
     //const nearestSq = n => Math.power(Math.round(Math.sqrt(n)), 2)
 /********************************************************************* */
+// There is a bus moving in the city which takes and drops some people at each bus stop.
+
+// You are provided with a list (or array) of integer pairs.
+//Elements of each pair represent the number of people that get on the bus (the first item) and the number of people that get off the bus (the second item) at a bus stop.
+
+// Your task is to return the number of people who are still on the bus after the last bus stop (after the last array).
+//Even though it is the last bus stop, the bus might not be empty and some people might still be inside the bus, they are probably sleeping there :D
+
+// Take a look on the test cases.
+
+// Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the returned integer can't be negative.
+
+// The second value in the first pair in the array is 0, since the bus is empty in the first bus stop
+
+    //const remainingPassengers = arr = arr.reduce((remaining, [on, off])=> remaining + on - off, 0)
 /********************************************************************* */
+//create a function to check if obj is empty
+    // function isEmpty(obj){
+    //     for(let key in obj){
+    //         return false
+    //     }
+    //     return true
+    // }
 /********************************************************************* */
 /********************************************************************* */
 /********************************************************************* */
