c9760140c22e2daf830b4042f93233d2b667b886
dragvn <liminalinquiries@gmail.com>
2023-03-16 19:37:42 -0400

    anki

--- main.js
+++ main.js
@@ -1445,23 +1445,42 @@ ARRAY METHODS
     //const pairZeros = (arr,f) => arr.filter(n=> n || (f = !f))
 /********************************************************************* */
 //declare class 'MakeCar' with 4 properties and 2 methods
-class MakeCar {
-    constructor(make,model,color,doors){
-        this._make = make,
-        this._model = model,
-        this.color = color,
-        this._doors = doors
-    }
-        honk(){
-            console.log('bbbepep')
-        }
-        lock(){
-            console.log(`locked it tahe fuckk up ${this._doors} door style`)
-        }
-}
-let scrujb = new MakeCar('mazonda', 'proplyyi', 'hot pink', 3)
+    // class MakeCar {
+    //     constructor(make,model,color,doors){
+    //         this._make = make,
+    //         this._model = model,
+    //         this.color = color,
+    //         this._doors = doors
+    //     }
+    //         honk(){
+    //             console.log('bbbepep')
+    //         }
+    //         lock(){
+    //             console.log(`locked it tahe fuckk up ${this._doors} door style`)
+    //         }
+    // }
+    // let scrujb = new MakeCar('mazonda', 'proplyyi', 'hot pink', 3)
 /********************************************************************* */
+// There is a bus moving in the city which takes and drops some people at each bus stop.
+
+// You are provided with a list (or array) of integer pairs.
+//Elements of each pair represent the number of people that get on the bus (the first item) 
+//and the number of people that get off the bus (the second item) at a bus stop.
+
+// Your task is to return the number of people who are still on the bus after the last bus stop (after the last array).
+//Even though it is the last bus stop, the bus might not be empty and some people might still be inside the bus, 
+//they are probably sleeping there :D
+
+// Take a look on the test cases.
+
+// Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. 
+//So the returned integer can't be negative.
+
+// The second value in the first pair in the array is 0, since the bus is empty in the first bus stop
+
+    //const remainingPassengers = arr = arr.reduce((remaining, [on, off])=> remaining + on - off, 0)
 /********************************************************************* */
+
 /********************************************************************* */
 /********************************************************************* */
 /********************************************************************* */
