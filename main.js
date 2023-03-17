0f4de7b55d2afe3d34b2c2c31841e4cbdc374ef6
dragvn <liminalinquiries@gmail.com>
2023-03-17 08:37:02 -0400

    -anki

--- main.js
+++ main.js
@@ -1480,7 +1480,11 @@ ARRAY METHODS
 
     //const remainingPassengers = arr = arr.reduce((remaining, [on, off])=> remaining + on - off, 0)
 /********************************************************************* */
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter.
+//Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+// If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
 
+    //const findNextSquare = n => Number.isInteger(Math.sqrt(n)) ? Math.pow(Math.round(Math.sqrt(n) + 1), 2) : -1
 /********************************************************************* */
 /********************************************************************* */
 /********************************************************************* */
