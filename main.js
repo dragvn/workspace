22ddd586084ba8ae537b85721673c4abb9d457f5
dragvn <liminalinquiries@gmail.com>
2023-03-25 07:45:06 -0400

    ztu

--- main.js
+++ main.js
@@ -1547,8 +1547,24 @@ ARRAY METHODS
 // Pairing two 0s generates one 0 at the location of the first one.
     //const pairZeros = (arr, f) => arr.filter(n => n || (f = !f))
 /********************************************************************* */
+// You are provided with a list (or array) of integer pairs.
+//Elements of each pair represent the number of people that get on the bus (the first item) and the number of people that get off the bus (the second item) at a bus stop.
+
+// Your task is to return the number of people who are still on the bus after the last bus stop (after the last array).
+//Even though it is the last bus stop, the bus might not be empty and some people might still be inside the bus, they are probably sleeping there :D
+
+// Take a look on the test cases.
+
+// Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the returned integer can't be negative.
+
+// The second value in the first pair in the array is 0, since the bus is empty in the first bus stop
+    //const remainingPassengers = arr => arr.reduce((cur, [on, off]) => cur + on - off, 0)
 /********************************************************************* */
+//find nearest square to provided num
+    //const nearestSq = n => Math.power(Math.round(Math.sqrt(n)), 2)
 /********************************************************************* */
+//Return the century for a given year
+    //const century = year => Math.ceil(year/100)
 /********************************************************************* */
 /********************************************************************* */
 /********************************************************************* */
