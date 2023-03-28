6b70a41c4ed20b67e7958533361612a85c475c81
dragvn <liminalinquiries@gmail.com>
2023-03-28 08:27:19 -0400

    SHWEET

--- main.js
+++ main.js
@@ -1575,6 +1575,13 @@ ARRAY METHODS
 
 //
 /********************************************************************* */
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter.
+//Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+// If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
+    // const findNextSquare = n => Number.isInteger(Math.sqrt(n)) ? Math.pow(Math.sqrt(n) + 1, 2) : -1
+
+    // console.log(findNextSquare(4))                           NICE WORK!!!!
+    // console.log(findNextSquare(5))
 /********************************************************************* */
 /********************************************************************* */
 /********************************************************************* */
