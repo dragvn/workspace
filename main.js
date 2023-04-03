0e74a11f3fe823e6585dbd7a67fbd68224006c58
dragvn <liminalinquiries@gmail.com>
2023-04-03 14:58:28 -0400

    ztyu

--- main.js
+++ main.js
@@ -1667,7 +1667,20 @@ ARRAY METHODS
     // console.log(findObjInArrByAge(arrOfObj, 32))
     // console.log(findObjInArrByAge(arrOfObj, 22))
 /********************************************************************* */
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
 
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples: (Input --> Output)
+
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+    // const lineNumbers = str => str.map((n, i)=> `${i+1}: ` + n )
+
+    // console.log(lineNumbers(["a", "b", "c"]))
 /********************************************************************* */
 /********************************************************************* */
 /********************************************************************* */
