fc4983df3d2c0550c19dbdf3c72b6ea51ff17759
dragvn <liminalinquiries@gmail.com>
2023-04-06 06:52:17 -0400

    prepended string

--- main.js
+++ main.js
@@ -1728,6 +1728,18 @@ ARRAY METHODS
 // Implement a function that adds two numbers together and returns their sum in binary
     //const binarySum = (a, b) => (a + b).toString(2)
 /********************************************************************* */
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples: (Input --> Output)
+
+// [] --> []    
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+    //const lineNumbers = str => str.map((c, i)=> `${i+1}: ${c}`)
 /********************************************************************* */
 /********************************************************************* */
 /********************************************************************* */
