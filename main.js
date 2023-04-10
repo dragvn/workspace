fd8f84bdf108874e010d541c24433064d40563e9
dragvn <liminalinquiries@gmail.com>
2023-04-10 09:07:29 -0400

    cartographer

--- main.js
+++ main.js
@@ -1741,6 +1741,19 @@ ARRAY METHODS
 
     //const lineNumbers = arr => arr.map((c, i)=> `${i+1}: ${c}`)
 /********************************************************************* */
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples: (Input --> Output)
+
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+const appendStr = arr => arr.map((v,i)=> `${i+1}: ${v}`)
+
 /********************************************************************* */
 /********************************************************************* */
 /********************************************************************* */
