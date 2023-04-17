c0e0e80854653800c416db6166c594e7ec24b516
dragvn <liminalinquiries@gmail.com>
2023-04-17 10:22:45 -0400

    merge sort

--- main.js
+++ main.js
@@ -1922,6 +1922,33 @@ ARRAY METHODS
 
     //const appendArrOfStr = arr => arr.map((v,i)=> `${i + 1}: ${v}`)
 /********************************************************************* */
+//MERGE SORT
+function mergeSort(arr){
+    if(arr.length < 2){
+      return arr
+    }
+    const middleIndex = Math.floor(arr.length / 2)
+    const leftHalf = arr.slice(0, middleIndex)
+    const rightHalf = arr.slice(middleIndex, arr.length) //slice doesnt include last val so no need for -1
+    return merge(mergeSort(leftHalf), mergeSort(rightHalf))
+}
+
+function merge(leftArr, rightArr){
+    let resultArr = []
+    let leftIndex = 0
+    let rightIndex = 0
+
+    while(leftIndex < leftArr.length && rightIndex < rightArr.length){
+        if(leftArr[leftIndex] < rightArr[rightIndex]){
+            resultArr.push(leftArr[leftIndex])
+            leftIndex +=1 
+        }else{
+            resultArr.push(rightArr[rightIndex])
+            rightIndex += 1
+        }
+    }
+    return resultArr.concat(leftArr.slice(leftIndex)).concat(rightArr.slice(rightIndex))
+}
 /********************************************************************* */
 /********************************************************************* */
 /********************************************************************* */
