eace081ab9c6c846c940f11e10db73ca389855d1
dragvn <liminalinquiries@gmail.com>
2023-04-26 11:08:54 -0400

    sortByAge

--- main.js
+++ main.js
@@ -2374,6 +2374,17 @@ usersMapped = [
     // }
     // console.log(oddEvens(str))
 /********************************************************************* */
+// // Write the function sortByAge(users) that gets an array of objects with the age property and sorts them by age.
+
+// // For instance:
+
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+
+// let arr = [ pete, john, mary ];
+
+//const sortByAge = arr => arr.sort((a, b) => a.age - b.age)
 /********************************************************************* */
 /********************************************************************* */
 /********************************************************************* */
