674c50d5899464f2e9f10b7d229ffffff3700ba9
dragvn <liminalinquiries@gmail.com>
2023-04-26 11:33:31 -0400

    usersMapped

--- jsMethods/arrayMethods.js
+++ jsMethods/arrayMethods.js
@@ -95,6 +95,28 @@
 
 //const orderedArrayOfStrings = arr => arr.sort((a, b)=> a.length - b.length)
 /************************************************************************************************************** */
+// You have an array of user objects, each one has name, surname and id.
+
+// Write the code to create another array from it, of objects with id and fullName, where fullName is generated from name and surname.
+
+// For instance:
+
+// let john = { name: "John", surname: "Smith", id: 1 };
+// let pete = { name: "Pete", surname: "Hunt", id: 2 };
+// let mary = { name: "Mary", surname: "Key", id: 3 };
+
+// let users = [ john, pete, mary ];
+
+// const usersMapped = users.map((user => ({fullName: `${user.name} ${user.surname}`, id: user.id})))
+// console.log(usersMapped)
+
+
+
+
+
+
+
+
 
 /************************************************************************************************************** */
 /************************************************************************************************************** */
