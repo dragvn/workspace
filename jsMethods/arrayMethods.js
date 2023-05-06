ec6223f3348dc12eaaaeaea1529cbf2a1dcab6bf
dragvn <liminalinquiries@gmail.com>
2023-05-05 20:52:03 -0400

    sort array of strings

--- jsMethods/arrayMethods.js
+++ jsMethods/arrayMethods.js
@@ -109,16 +109,39 @@
 
 // const usersMapped = users.map((user => ({fullName: `${user.name} ${user.surname}`, id: user.id})))
 // console.log(usersMapped)
-
-
-
-
-
-
-
-
-
 /************************************************************************************************************** */
+// Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+
+// For example, if this array were passed as an argument:
+
+// ["Telescopes", "Glasses", "Eyes", "Monocles"]
+
+// Your function would return the following array:
+
+// ["Eyes", "Glasses", "Monocles", "Telescopes"]
+
+// All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
+
+//PARAMS
+    //array of strings
+//RETURN
+    //sorted array (by str length)
+//EXAMPLE
+    //["Telescopes", "Glasses", "Eyes", "Monocles"] --> ["Eyes", "Glasses", "Monocles", "Telescopes"]
+//PSEUDO
+    //just try to sort iit <--- this is the way my son
+
+// let arr = ["Telescopes", "Glasses", "Eyes", "Monocles"]  
+
+// const sortedArrayOfString = arr => arr.sort((a, b) => a.length - b.length)
+
+// console.log(sortedArrayOfString(arr))
+
+
+
+
+
+
 /************************************************************************************************************** */
 /************************************************************************************************************** */
 /************************************************************************************************************** */
