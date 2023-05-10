9e55a992137e6ae5894e2000834d481621e7f019
dragvn <liminalinquiries@gmail.com>
2023-05-10 16:02:41 -0400

    sortByAge

--- main.js
+++ main.js
@@ -2418,7 +2418,43 @@ usersMapped = [
 // console.log(isAnagram(thisStr, thatStr))
 // console.log(isAnagram(thisStr, otherStr))
 /********************************************************************* */
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter.
+// Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+// If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
+
+//params
+    //num
+//return
+    //-1 if param is not perfect square, or num (next perf square) so -1 or num
+//ex
+    //(3) --> -1
+    //(4) --> 9
+//pseudo
+    //if param is not perf sq return -1
+    //sqrt num, add 1, square it
+
+    // const findNextSquare = num => Number.isInteger(Math.sqrt(num)) ? Math.pow(Math.sqrt(num)+ 1, 2) : -1
+    // console.log(findNextSquare(4))
+    // console.log(findNextSquare(3))
 /********************************************************************* */
+// // Write the function sortByAge(users) that gets an array of objects with the age property and sorts them by age.
+
+// // For instance:
+
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+
+// let arr = [ pete, john, mary ];
+
+//param
+    //arr of obj
+//return
+    //arr of obj sorted by age
+//ex
+    //[john, mary, pete]
+
+//const sortByAge = arr => arr.sort((a, b)=> a.age - b.age)
 /********************************************************************* */
 /********************************************************************* */
 /********************************************************************* */
